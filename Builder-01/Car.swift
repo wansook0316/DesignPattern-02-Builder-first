43a5168d9e9c2bfc0f61f34cebcced1161da8356
wansook0316 <wansook0316@gmail.com>
2022-08-09 17:14:52 +0900

    Add: Car

--- Builder-01/Car.swift
+++ Builder-01/Car.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+internal struct Car {
+    internal func describe() -> String {
+        return """
+        Car Spec
+        brandName: \(brandName)
+        engine: \(engine)
+        airbag: \(airbag)
+        color: \(color)
+        isSelfDrivingAvailable: \(isSelfDrivingAvailable)
+        isSexy: \(isSexy)
+        """
+    }
+    
+    private var brandName: String
+    private var engine: String
+    private var airbag: String
+    private var color: String
+    private var isSelfDrivingAvailable: Bool
+    private var isSexy: Bool
+}
