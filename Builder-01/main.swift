16f5b8d80cd5754367965d7ec0e1ed40cf7ec971
wansook0316 <wansook0316@gmail.com>
2022-08-09 17:36:59 +0900

    Update: Builder가 용이한 이유 추가

--- Builder-01/main.swift
+++ Builder-01/main.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 internal func main() {
+    Car와CarBuilder호출방식차이확인하기()
+    CarBuilder가재사용에용이한이유알아보기()
+}
+
+internal func Car와CarBuilder호출방식차이확인하기() {
+    print("+++ Car와 CarBuilder 호출방식 차이 확인하기 +++")
+    
     // initializer를 사용해서 처리하기
     let car1 = Car(brandName: "Ford Mustang",
                   engine: "V8",
@@ -28,6 +35,39 @@ internal func main() {
         .build()
     
     print(car2.description)
+}
+
+internal func CarBuilder가재사용에용이한이유알아보기() {
+    print("+++ CarBuilder가 재사용에 용이한 이유 알아보기 +++")
+    
+    let teslaDefault = CarBuilder()
+        // .brandName("Tesla")
+        .engine("Electric motor")
+        .airbag(true)
+        .color("black")
+        .isSelfDrivingAvailable(true)
+        .isSexy(false)
+    
+    let modelS = teslaDefault
+        .brandName("model S")
+        .build()
+    
+    let model3 = teslaDefault
+        .brandName("model 3")
+        .build()
+    
+    let modelX = teslaDefault
+        .brandName("model X")
+        .build()
+    
+    let modelY = teslaDefault
+        .brandName("model Y")
+        .build()
+    
+    print(modelS.description)
+    print(model3.description)
+    print(modelX.description)
+    print(modelY.description)
 }
 
 main()
