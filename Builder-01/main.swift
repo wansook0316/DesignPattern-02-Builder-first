7ab0826f4dd63aede330b1ab6d5c13dbb987e1f0
wansook0316 <wansook0316@gmail.com>
2022-08-09 17:31:47 +0900

    Update: Car, Carbuilder 차이 확인 위해 main 추가

--- Builder-01/main.swift
+++ Builder-01/main.swift
@@ -7,5 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+internal func main() {
+    // initializer를 사용해서 처리하기
+    let car1 = Car(brandName: "Ford Mustang",
+                  engine: "V8",
+                  airbag: true,
+                  color: "red",
+                  isSelfDrivingAvailable: false,
+                  isSexy: false)
+    print(car1.description)
+    
+    // Builder를 사용해서 처리하기
+    let car2 = CarBuilder()
+        .brandName("Tesla")
+        .engine("Electric motor")
+        .airbag(true)
+        .color("black")
+        .isSelfDrivingAvailable(true)
+        .isSexy(false)
+        .build()
+    
+    print(car2.description)
+}
 
+main()
